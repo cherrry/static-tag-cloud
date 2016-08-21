f41ef3484f2c0d9d1edcf51c30788e840f238be1
Cherry Ng <cherry92@gmail.com>
2016-08-22 07:14:37 +0800

    Fix hashing issue that caused poor shuffling

--- src/string-hash.js
+++ src/string-hash.js
@@ -1,8 +1,8 @@
 'use strict'
 
 // Two carefully selected primes
-var P1 = 1955977
-var P2 = 21273479
+var P1 = 43063
+var P2 = 48611
 
 var stringHash = function (string, p1, p2) {
   p1 = p1 || P1
@@ -11,7 +11,7 @@ var stringHash = function (string, p1, p2) {
   var hash = 0
   for (var i = 0, _len = string.length; i < _len; i++) {
     var code = string.charCodeAt(i)
-    hash = (((hash * P1) % P2) + code) % P2
+    hash = ((((hash * p1) % p2) + code) * p1) % p2
   }
   return hash
 }
